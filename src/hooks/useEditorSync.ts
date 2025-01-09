bb218ebb6ff06d90ad550f25893da88c6acf6502
daniel-lou18 <danielderudder@hotmail.com>
2025-01-09 17:54:28 +0100

    fix(custom hook): fix hook that syncs UI state to server state, create a ref to avoid sync triggering on every change in the pendingChanges set that is part of the reducer

--- src/hooks/useEditorSync.ts
+++ src/hooks/useEditorSync.ts
@@ -2,35 +2,38 @@ import {
   Action as EditorAction,
   InitialState as EditorState,
 } from "@/contexts/editorContext";
-import { Dispatch, useCallback, useEffect } from "react";
+import { Dispatch, useEffect, useRef } from "react";
 import { useSaveSegments } from "./useSaveSegments";
 
 export function useEditorSync(
   state: EditorState,
   dispatch: Dispatch<EditorAction>
 ) {
-  const { mutate, savedSegmentIds } = useSaveSegments();
-
-  const syncChanges = useCallback(() => {
-    if (state.pendingChanges.size === 0) return;
-
-    const changedSegments = state.segments.filter((segment) =>
-      state.pendingChanges.has(segment.id)
-    );
-
-    mutate(changedSegments, {
-      onSuccess: () => dispatch({ type: "SYNC_COMPLETED" }),
-    });
-
-    console.log(savedSegmentIds);
-  }, [mutate, state.pendingChanges, state.segments, dispatch, savedSegmentIds]);
+  const stateRef = useRef(state);
+  const { mutate } = useSaveSegments();
 
   useEffect(() => {
-    const intervalId = setInterval(syncChanges, 30000);
-    return () => clearInterval(intervalId);
-  }, [syncChanges]);
+    stateRef.current = state;
+  }, [state]);
 
   useEffect(() => {
-    return () => syncChanges();
-  }, [syncChanges]);
+    const syncChanges = () => {
+      if (stateRef.current.pendingChanges.size === 0) return;
+
+      const changedSegments = stateRef.current.segments.filter((segment) =>
+        stateRef.current.pendingChanges.has(segment.id)
+      );
+
+      mutate(changedSegments, {
+        onSuccess: () => dispatch({ type: "SYNC_COMPLETED" }),
+      });
+    };
+
+    const intervalId = setInterval(syncChanges, 15000);
+
+    return () => {
+      syncChanges();
+      clearInterval(intervalId);
+    };
+  }, [dispatch, mutate]);
 }
